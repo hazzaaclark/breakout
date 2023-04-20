a24c138feab3fa83fa99209b66118d433a126835
Harry <107435091+hazzaaclark@users.noreply.github.com>
2023-04-20 15:41:20 +0100

    Add files via upload

---
+++ main.s
@@ -0,0 +1,19 @@
+;----------------------------------------------------------
+;            SEGA MEGA DRIVE CLONE OF BREAKOUT
+;                  (C) HARRY CLARK 2023
+;----------------------------------------------------------
+
+;----------------------------------------------------------
+;     THIS SECTION PERTAINS TO THE MAIN FUNCTIONALITY OF
+;          THE PROGRAM; USING THE METHODS DEFINED FROM
+;                     THE OTHER FILES
+;----------------------------------------------------------
+
+INCLUDE: system.s
+
+.SECTION .TEXT
+
+.GLOBAL _MAIN
+
+_MAIN:
+
