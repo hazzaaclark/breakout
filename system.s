045331b3a65694443cda966804eefb4834386106
Harry <107435091+hazzaaclark@users.noreply.github.com>
2023-04-20 20:13:24 +0100

    Update system.s

--- system.s
+++ system.s
@@ -11,7 +11,10 @@
 ;----------------------------------------------------------
 
 ;----------------------------------------------------------
-; DOCUMENTATION USED: https://www.nxp.com/docs/en/reference-manual/MC68000UM.pdf
+; DOCUMENTATION USED: 
+; https://www.nxp.com/docs/en/reference-manual/MC68000UM.pdf
+;
+; https://www.copetti.org/writings/consoles/mega-drive-genesis/
 ;----------------------------------------------------------
 
 .GLOBAL: SYSTEM
@@ -29,6 +32,11 @@ VDP_INIT:    EQU   $00000000
 VDP_STATUS:  EQU   $00000004
 VDP_CTRL:    EQU   $00000004
 
+VRAM:        EQU   %01* BIG_ENDIAN +% 0000 * LITLE_ENDIAN
+VSRAM:       EQU   %01* BIG_ENDIAN +% 0000 * LITLE_ENDIAN
+CRAM:        EQU   %11* BIG_ENDIAN +% 0000 * LITLE_ENDIAN
+STACK_SIZE:  EQU   1024
+
 ;-----------------------------------------------------------
 
 ;-----------------------------------------------------------
@@ -41,6 +49,15 @@ DMA_WAIT:          MACRO
 @LOOP\@            BTST 1, VDP_STATUS+1 // ALLOCATE A BIT NUMBER TO THE DESTINATION OF THE VDP_STATUS
                    BNE.S @LOOP\@ // JUMP TO THIS VARIABLE IF THE ZERO FLAG IS CLEAR INSIDE OF THE SUPERVISOR STATE 
                                 // END THE LOOP AFTER
+                   ENDM
+
+// SIMILAR TO A SHIFT, WHEN DEALING WITH DMA, WHEN THE LAST CORRESPONDING VALUE IS AT THE END
+// ROTATE IT BACK AROUND TO A PARTICULAR DESTINATION BY A DETERMINE AMOUNT, (B, W, L)
+
+DMA_CONTROL:       MACRO
+                   ROL.L #2, \2 
+                   ROR   #2, \2
+                   SWAP \2 // SWAP INTO THE FIRST 16 BITS
                    ENDM
 
 ;-----------------------------------------------------------
