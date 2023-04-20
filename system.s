13cc21063adf7382067048ce50483c1cbc1bc38c
Harry <107435091+hazzaaclark@users.noreply.github.com>
2023-04-20 15:52:28 +0100

    Update system.s

--- system.s
+++ system.s
@@ -10,10 +10,27 @@
 ;        TO HELP PARSE INFORMATION THROUGH THE CONSOLE
 ;----------------------------------------------------------
 
-.SECTION: 
+.SECTION: REGISTERS
 
-VDP_INIT:          EQU $0000000
+;-----------------------------------------------------------
+
+REGISTERS:
+
+
+VDP_INIT:    EQU   $00000000 
+VDP_STATUS:  EQU   $00000004
+
+;-----------------------------------------------------------
+
+;-----------------------------------------------------------
+
+.SECTION: SYSTEM_MACROS
+
+SYSTEM_MACROS:
 
 DMA_WAIT:          MACRO
-@LOOP\@            BTST
-DMA_WAIT:          ENDM
+@LOOP\@            BTST 1, VDP_STATUS+1 // ALLOCATE A BIT NUMBER TO THE DESTINATION OF THE VDP_STATUS
+                   BNE.S @LOOP\@ // JUMP TO THIS VARIABLE IF THE ZERO FLAG IS CLEAR; END THE LOOP AFTER
+                   ENDM
+
+;-----------------------------------------------------------
