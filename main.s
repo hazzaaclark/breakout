92f6477ebff7c40f1f3226682db75ba334a18fb6
Harry <107435091+hazzaaclark@users.noreply.github.com>
2023-04-22 14:55:03 +0100

    Update main.s

--- main.s
+++ main.s
@@ -16,4 +16,31 @@ INCLUDE: GAME_LOGIC
 
 .GLOBAL: MAIN
 
+;----------------------------------------------------------
+;                 VRAM MEMORY MAPPING
+;----------------------------------------------------------
+
+BLANK_CHAR:      EQU     $0000-$00200
+SPRITE_CHAR:     EQU     $0020-$8000
+CHAR_MAP:        EQU     4096
+
+;----------------------------------------------------------
+;        INTILIASE THE VDP THROUGH THE SYSTEM
+;                   AND GAME LOGIC
+;----------------------------------------------------------
+;----------------------------------------------------------
+;
+;   CREATE TEMPORARY BUFFERS FOR THE GAME INITIALISATION
+;           TO DRAW THE GRAPHICS ON SCREEM
+;----------------------------------------------------------
+
+VDP_INIT:        MOVE.L    #0    PLAYER_X
+                 MOVE.W    #1    PLAYER_Y
+
+                 DMA_DUMP_DATA    #4*32, $20 ;; DUMP THE DMA CHARACTERS BY 32 BYTES INTO REGISTER 20     
+
+;----------------------------------------------------------
+
+
+
 MAIN:
