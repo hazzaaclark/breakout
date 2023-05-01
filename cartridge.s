39b00c5f237a34c252ca3568c92c2d2af119f473
Harry <107435091+hazzaaclark@users.noreply.github.com>
2023-05-01 23:30:30 +0100

    Add files via upload

---
+++ cartridge.s
@@ -0,0 +1,20 @@
+;----------------------------------------------------------
+;            SEGA MEGA DRIVE CLONE OF BREAKOUT
+;                  (C) HARRY CLARK 2023
+;----------------------------------------------------------
+
+;----------------------------------------------------------
+;     THIS FILE PERTAINS TO THE DECLARATIONS OF
+;             THE CARTRIDGE INFORMATION
+;----------------------------------------------------------
+
+;----------------------------------------------------------
+; THIS IS JUST A GENERAL PRACTISE USED WHEN WRITING SOFTWARE
+; FOR MEGA DRIVE GAMES AS SHOWCASED BY DECOMP'S OF MD GAMES
+;----------------------------------------------------------
+
+.GLOBAL: CARTRIDGE
+
+CARTRIDGE:
+
+ORG:              $0000 ;; DEFINE A 4 BYTE STACK LOCATION COUNTER
